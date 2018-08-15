ce78d40d4315b62a119fd83f895430e2e785ddc1
timichal <michal@zlatkovsky.cz>
2018-08-15 16:41:37 +0200

    js fix

--- js/targetblank.js
+++ js/targetblank.js
@@ -1,6 +1,6 @@
 // copy https://code.luasoftware.com/tutorials/hugo/how-to-create-link-with-target-blanks-in-hugo-markdown/
 
-var links = content.getElementsByTagName('a');
+var links = document.getElementsByTagName('a');
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
    if (links[i].hostname != window.location.hostname) {
       links[i].target = '_blank';
